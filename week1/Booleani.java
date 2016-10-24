eab706bb2c166ab0f80dbdf6a1ced4b4e0f4d942
maboglia <mauro.bogliaccino@gmail.com>
2016-10-24 17:49:09 +0200

    primo invio di codice

---
+++ week1/Booleani.java
@@ -0,0 +1,24 @@
+package esempi.week1;
+
+public class Booleani {
+
+	public static void main(String[] args) {
+
+		boolean b1,b2,b3;
+		
+		b1 = 3 > 5;
+		
+		b2 = 1 < 2;
+		
+		b3 = b1 & b2;
+		
+		System.out.println(b3);
+		
+		b3 = b1 | b2;
+		
+		System.out.println(b3);
+		
+		
+	}
+
+}
