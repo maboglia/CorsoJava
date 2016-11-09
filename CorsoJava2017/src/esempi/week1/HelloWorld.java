e5eff37e397efeb427179b784f51eb3f2a26eaa7
maboglia <mauro.bogliaccino@gmail.com>
2016-11-09 15:45:05 +0100

    test

---
+++ CorsoJava2017/src/esempi/week1/HelloWorld.java
@@ -0,0 +1,12 @@
+package esempi.week1;
+
+public class HelloWorld{
+	//corpo della classe HelloWorld
+
+	public static void main(String[] args){
+		//corpo del metodo main
+		System.out.println("Hello, world!");
+
+	}
+
+}
