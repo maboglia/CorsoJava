4a3a9f9cc4ef0befd6782cd32f186686093df7ff
maboglia <mauro.bogliaccino@gmail.com>
2016-11-14 13:38:01 +0100

    geom

---
+++ CorsoJava2017/src/esempi/week5/geometria/Rettangolo.java
@@ -0,0 +1,35 @@
+package esempi.week5.geometria;
+
+/**
+ * Created by mauro on 23/06/16.
+ */
+public class Rettangolo {
+
+    private Punto angoloSx = null;
+
+    private int base, altezza;
+
+    public Rettangolo(int x, int y, int b, int h){
+
+        angoloSx    = new Punto(x,y);
+        base        = b;
+        altezza     = h;
+
+    }
+    public Rettangolo(Punto p, int b, int h){
+
+        angoloSx    = new Punto(p.getX(), p.getY());
+        base        = b;
+        altezza     = h;
+
+    }
+    public int getPerimetro(){
+        return (base + altezza) * 2;
+    }
+    public int getArea(){
+        return base * altezza;
+    }
+
+
+
+}
