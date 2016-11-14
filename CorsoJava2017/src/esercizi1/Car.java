9169108fe433fd973a2a6681efcb08cf97d47821
maboglia <mauro.bogliaccino@gmail.com>
2016-11-14 17:49:42 +0100

    car

---
+++ CorsoJava2017/src/esercizi1/Car.java
@@ -0,0 +1,23 @@
+package esercizi1;
+
+public class Car {
+
+	
+	/*
+	 * Progettare e realizzare una classe Car(automobile) con le proprietà seguenti. 
+	 * Un’automobile ha una determinata resa del carburante 
+	 * (misurata in miglia/galloni o in litri/chilometri: scegliete il sistema che preferite) 
+	 * e una certa quantità di carburante nel serbatoio. 
+	 * La resa è specificata dal costruttore e il livello iniziale del carburante è a zero. 
+	 * Fornire questi metodi: un metodo drive per simulare il percorso 
+	 * di un’automobile per una certa distanza, riducendo il livello di carburante nel serbatoio; 
+	 * un metodo getGas, per ispezionare il livello corrente del carburante; 
+	 * un metodo addGas per far rifornimento.
+		Consigli:
+		S’invita a porre particolare attenzione al metodo drive(double km), 
+		che calcola il livello di carburante dopo un certo percorso.
+	 * */
+	
+	
+	
+}
