5b634d93d021c010cef6065cb0770426821c9c36
maboglia <mauro.bogliaccino@gmail.com>
2016-11-15 09:25:58 +0100

    implemento car

--- CorsoJava2017/src/esercizi1/Car.java
+++ CorsoJava2017/src/esercizi1/Car.java
@@ -18,6 +18,31 @@ public class Car {
 		che calcola il livello di carburante dopo un certo percorso.
 	 * */
 	
-	
+	// quantità di carburante nel serbatoio
+	private double gas;
+	// prestazioni della vettura
+	private double kmL;
+	// si costruisce un'automobile con carburante uguale a zero
+	public Car(double resa)
+	{
+	kmL = resa;
+	gas = 0; }
+	/* Calcola il livello di carburante rimasto dopo un certo percorso
+	@param km i chilometri percorsi*/
+	public void drive(double km)
+	{
+	      gas = gas - (km / kmL);
+	}
+	/* ispeziona il livello di carburante rimasto
+	@return il carburante rimasta*/
+	public double getGas()
+	{
+	return gas; }
+	/* aggiunge carburante nel serbatoio
+	@param rifornimento il carburante da aggiungere*/
+	public void addGas(double rifornimento)
+	{
+	      gas = gas + rifornimento;
+	}
 	
 }
