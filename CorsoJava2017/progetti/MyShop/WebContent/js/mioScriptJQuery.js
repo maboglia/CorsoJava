292011d139452194fef3fca59fdd85259513d244
maboglia <mauro.bogliaccino@gmail.com>
2017-02-20 17:23:12 +0100

    Add files via upload

--- CorsoJava2017/progetti/MyShop/WebContent/js/mioScriptJQuery.js
+++ CorsoJava2017/progetti/MyShop/WebContent/js/mioScriptJQuery.js
@@ -6,12 +6,61 @@
 //     elem.appendChild(para);
 // });
 
+var Prodotti = [
+	
+	{ 
+		"Abito" 	: "Camicia",
+		"Taglia"	: ["S", "M", "L", "XL"],
+		"Colore"	: ["Rosso", "Verde", "Blue"],
+		"Prezzo"	: 39.99
+	},
+
+	{ 
+		"Abito" 	: "Maglione",
+		"Taglia"	: ["S", "M", "L", "XL"],
+		"Colore"	: ["Rosso", "Verde", "Blue"],
+		"Prezzo"	: 49.99
+	},
+
+	{ 
+		"Abito" 	: "Giacca",
+		"Taglia"	: ["S", "M", "L", "XL"],
+		"Colore"	: ["Rosso", "Verde", "Blue"],
+		"Prezzo"	: 69.99
+	},
+
+	{ 
+		"Abito" 	: "Pantalone",
+		"Taglia"	: ["S", "M", "L", "XL"],
+		"Colore"	: ["Rosso", "Verde", "Blue"],
+		"Prezzo"	: 59.99
+	}
+
+
+
+];
+
+
+$.each(Prodotti, function(index, val) {
+	 //alert(val.Abito + " " + val.Prezzo);
+	var mioElemento = "<div class='prodotto'>"+ val.Abito +  " " + val.Taglia[0] + "</div>";
+	 $("#content").append(mioElemento);
+	 
+	 
+});
+
+$(".prodotto").css("width", "300px").css("height", "300px").css("border", "1px dotted blue");
+
+
 $.getJSON( "js/dario_mennillo.json", function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
     items.push( "<li id='" + key + "'>" + val + "</li>" );
   });
  
+  
+  
+  
   $( "<ul/>", {
     "class": "my-new-list",
     html: items.join( "" )
