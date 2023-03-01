2f5a96cbb2324a1aa9e9663a9b1893b3b4a11dd2
maboglia <mauro.bogliaccino@gmail.com>
2023-03-01 15:54:55 +0100

    agg

--- esercizi/soluzioni/Istogramma.java
+++ esercizi/soluzioni/Istogramma.java
@@ -1,6 +1,6 @@
 /* Programma per disegnare l'istogramma di un array
    di interi */
-class E11_Istogramma {
+class Istogramma {
   public static void main (String[] args) {
     int i, j;
     int[] frequenze = {3, 5, 9, 6, 7, 1, 20, 5, 9, 4, 3, 2};
