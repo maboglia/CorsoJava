40eb97c0fccc8f192985c146e191cb6af58a75b5
maboglia <mauro.bogliaccino@gmail.com>
2024-03-17 21:50:20 +0100

    agg

--- esempi/01_base/01_Operatori/001_OperazioniAritmetiche.java
+++ esempi/01_base/01_Operatori/001_OperazioniAritmetiche.java
@@ -1,32 +1,35 @@
-	public static void main(String[] args) {
-		
-		int i1 = 5, i2 =10, i3;
-		double d1 = 5, d2 =10, d3;
-		
-		i3 =- i1;
-		System.out.println(" -i1 = " + i3);
-		i3 = i1 + i2;
-		System.out.println("i1 + i2 = " + i3);
-		i3 = i1 - i2;
-		System.out.println("i1 - i2 = " + i3);
-		i3 = i1 * i2;
-		System.out.println("i1 * i2 = " + i3);
-		i3 = i1 / i2;
-		System.out.println("i1 / i2 = " + i3);
-		System.out.println("i1 * i2 = " + i3);
-		i3 = i1 % i2;
-		System.out.println("i1 % i2 = " + i3);
+public static void main(String[] args) {
+    // Dichiarazione e inizializzazione delle variabili intere
+    int i1 = 5, i2 = 10, i3;
 
-		d3 =- d1;
-		System.out.println(" -d1 = " + d3);
-		d3 = d1 + d2;
-		System.out.println("d1 + d2 = " + d3);
-		d3 = d1 - d2;
-		System.out.println("d1 - d2 = " + d3);
-		d3 = d1 * d2;
-		System.out.println("d1 * d2 = " + d3);
-		d3 = d1 / d2;
-		System.out.println("d1 / d2 = " + d3);
-		d3 = d1 % d2;
-		System.out.println("d1 % d2 = " + d3);
-	}
+    // Operazioni aritmetiche con interi
+    i3 = -i1;
+    System.out.println("-i1 = " + i3);
+    i3 = i1 + i2;
+    System.out.println("i1 + i2 = " + i3);
+    i3 = i1 - i2;
+    System.out.println("i1 - i2 = " + i3);
+    i3 = i1 * i2;
+    System.out.println("i1 * i2 = " + i3);
+    i3 = i1 / i2;
+    System.out.println("i1 / i2 = " + i3);
+    i3 = i1 % i2;
+    System.out.println("i1 % i2 = " + i3);
+
+    // Dichiarazione e inizializzazione delle variabili double
+    double d1 = 5, d2 = 10, d3;
+
+    // Operazioni aritmetiche con double
+    d3 = -d1;
+    System.out.println("-d1 = " + d3);
+    d3 = d1 + d2;
+    System.out.println("d1 + d2 = " + d3);
+    d3 = d1 - d2;
+    System.out.println("d1 - d2 = " + d3);
+    d3 = d1 * d2;
+    System.out.println("d1 * d2 = " + d3);
+    d3 = d1 / d2;
+    System.out.println("d1 / d2 = " + d3);
+    d3 = d1 % d2;
+    System.out.println("d1 % d2 = " + d3);
+}
